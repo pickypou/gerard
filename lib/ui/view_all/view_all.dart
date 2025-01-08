6dc0c2fd186d62f80e87f6daa63bb2d4c6b7d84b
NouveauNom <ludo.spysschaert@orange.fr>
2025-01-08 07:03:21 +0100

    creation dossier home-page, common,view-all, theme

---
+++ lib/ui/view_all/view_all.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:gerard/ui/home_page/home_page.dart';
+import 'package:gerard/ui/theme.dart';
+
+class ViewAll extends StatelessWidget {
+  const ViewAll({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    return SingleChildScrollView(
+      child: Container(
+        width: size.width,
+        height: size.height,
+        color: theme.primaryColor,
+        child:  const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+                child: HomePage())
+          ],
+        ),
+      ),
+    );
+  }
+}
