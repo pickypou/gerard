e68f56dfb7709c74547667d007d8e185793c79c5
pickypou <spysschaert.ludo@orange.fr>
2026-01-04 11:44:17 +0100

    feat: Add sitemap and robots.txt, update PWA manifest details, and enable clean URLs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:gerard/ui/theme.dart';
+import 'package:flutter_web_plugins/url_strategy.dart'; // Import nécessaire
 
 import 'core/di/di.dart';
 import 'core/router/router_config.dart';
 
-void main()  {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  usePathUrlStrategy(); // Activation des URLs propres
 
   configureDependencies();
 
   runApp(const MyApp());
-
-
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
